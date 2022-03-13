ff468548a6776d1d91899367db92c0ea5e515cf7
nequals30 <nequals30@gmail.com>
2022-03-13 18:18:21 -0500

    writing checks in localBackup function

--- localBackup.py
+++ localBackup.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
-from subprocess import check_output,call
-from datetime import datetime
-import fileinput
 
+import os
+from subprocess import check_output,call
+
+from datetime import datetime # for logging
+import fileinput # for logging
+
+# overhead for logging
 absolutePath = ""
 now = datetime.now()
 logPath = absolutePath + "logs/" + now.strftime("%Y-%m-%d") + "_log.md"
@@ -11,13 +15,51 @@ logPath = absolutePath + "logs/" + now.strftime("%Y-%m-%d") + "_log.md"
 try:
     # bulk of code (should be its own file, while the rest of this code is a generic wrapper for any script)
     # --------------------
-    raise Exception("guaranteed error")
+    pathToMainDrive = ""
+    pathToBackup = ""
+    maxDiff = 2 # gigabytes
+
+    # make sure both directories have trailing slashes
+    if not (pathToMainDrive.endswith("/") and pathToBackup.endswith("/")):
+        raise Exception("drive paths must have trailing slashes")
+
+    # check that both drives are a mount point
+    if not os.path.ismount(pathToMainDrive):
+        raise Exception(pathToMainDrive + " is not a mountpoint")
+    if not os.path.ismount(pathToBackup):
+        raise Exception(pathToBackup + " is not a mountpoint")
+        
+    # check that contents of both drives are within X GB of eachother
+    stats_main = os.statvfs(pathToMainDrive)
+    stats_back = os.statvfs(pathToBackup)
+    used_main = (stats_main.f_frsize * (stats_main.f_blocks - stats_main.f_bfree))/1e9
+    used_back = (stats_back.f_frsize * (stats_back.f_blocks - stats_back.f_bfree))/1e9
+    if abs(used_main - used_back) > maxDiff:
+        raise Exception("Drives are different by more than " + str(maxDiff) + "GB. Main drive has " + str(round(used_main,2)) + "GB, backup has " + str(round(used_back,2)) + "GB")
+    else:
+        successMessage = "main: " + str(round(used_main,2)) + "GB, backup: " + str(round(used_back,2)) + "GB. "
+
+    # check rsync dry-run
+    res = check_output(["rsync","--dry-run","--exclude=lost+found/","--archive","--delete","--stats",pathToMainDrive,pathToBackup])
+    res = res.decode("utf-8")
+    # to do: check if dry-run succeeded
+    for line in res.split('\n'):
+        if 'Total transferred file size: ' in line:
+            nBytes = int(line[line.find(': ')+2:line.rfind(' bytes')].strip().replace(",",""))
+        # to do, pull out stats on created, deleted
+    if abs(nBytes/1e9) > maxDiff:
+        raise Exception("Dry-run transferring " + str(round(nBytes/1e9,2)) + "GB (more than " + str(maxDiff) + "GB limit)")
+    else:
+        successMessage = successMessage + "dry-run: " + str(round(nBytes/1e9,2)) + " GB"
+
+    # run actual rsync
+    
     # --------------------
 
     # write success
     for line in fileinput.input(logPath,inplace=True):
         print(line.replace("| x | localBackup | HAS NOT RUN |",
-            "|" + now.strftime("%-I:%M %p") + "| localBackup | it ran |"),end='')
+            "|" + now.strftime("%-I:%M %p") + "| localBackup | " + successMessage + " |"),end='')
 except Exception as e:
     # write error
     for line in fileinput.input(logPath,inplace=True):
@@ -25,14 +67,7 @@ except Exception as e:
             ,"|" + now.strftime("%-I:%M %p") + "| localBackup | ERROR: " + str(e) + "|"),end='')
 
 """
-maxSize = 2e9
 
-# check how many bytes would be transferred
-res = check_output(["rsync","--dry-run","--exclude=lost+found/","--archive","--delete","--stats","/mnt/smallssd/","/mnt/bigDrive/"])
-res = res.decode("utf-8")
-for line in res.split('\n'):
-    if 'Total transferred file size: ' in line:
-        nBytes = int(line.split(":")[1].replace(" bytes","").strip().replace(",",""))
 
 # do transfer if it is OK, otherwise abort and send error
 print(str(nBytes/1e9) + " GB to be transferred")
