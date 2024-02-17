560a5970c0545270dfc2429a809913078cc0afe1
nequals30 <nequals30@gmail.com>
2024-02-17 13:37:37 -0600

    orchestration of local backup

--- 01_logger/start_log.py
+++ 01_logger/start_log.py
@@ -21,7 +21,7 @@ def main():
     f.write("|Time | Task | Message |\n")
     f.write("| --- | ---  | ---     |\n")
     f.write("| X | start_log | HAS NOT RUN |\n")
-    f.write("| X | localBackup | HAS NOT RUN |\n")
+    f.write("| X | local_backup | HAS NOT RUN |\n")
     f.close()
     return "Wrote this log with X scripts"
 
---
+++ orchestration/s02_local_backup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+from orchestration_tools import run_script
+
+run_script('02_local_backup','local_backup')
