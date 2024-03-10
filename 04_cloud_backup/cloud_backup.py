15d2f337d02b8716a5d077ecd1038f570776cc77
boss net <bossnet@bossnet.boss>
2024-03-10 10:03:33 -0500

    work on cloud backup

--- 04_cloud_backup/cloud_backup.py
+++ 04_cloud_backup/cloud_backup.py
@@ -3,6 +3,7 @@
 
 import subprocess
 import os
+import socket
 
 def main():
 
@@ -15,9 +16,12 @@ def main():
             k, v = line.strip().split("=",1)
             k = k.strip()
             v = v.strip()
+            if ',' in v:
+                v = [item.strip() for item in v.split(',')]
             config[k] = v
 
     backup_path = config.get("backup_path")
+    exclude_list = config.get("exclude_subdirs")
 
     # make sure the backup path is an absolute mount point
     if (not os.path.isabs(backup_path)): 
@@ -27,8 +31,22 @@ def main():
     if not os.path.ismount(backup_path):
         raise Exception(backup_path + " is not a mountpoint")
 
-    return "success or fail"
+    # get current machines hostname
+    hn = socket.gethostname()
 
+    # load restic secrets from pass into environment variables
+    os.environ['RESTIC_REPOSITORY'] = get_pass_entry(f"{hn}/RESTIC_REPOSITORY")
+    os.environ['B2_ACCOUNT_ID'] = get_pass_entry(f"{hn}/B2_ACCOUNT_ID")
+    os.environ['B2_ACCOUNT_KEY'] = get_pass_entry(f"{hn}/B2_ACCOUNT_KEY")
+    os.environ['RESTIC_PASSWORD'] = get_pass_entry(f"{hn}/RESTIC_PASSWORD")
+
+    temp_cmd = f"restic backup {backup_path} --compression max --exclude-caches --one-file-system --cleanup-cache"
+    for subdir in exclude_list:
+        temp_cmd += f" --exclude {backup_path}{subdir}"
+
+    print(temp_cmd)
+
+    return "success or fail"
 
 def get_pass_entry(entry_name):
     try:
